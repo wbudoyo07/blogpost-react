f19bd66377d1f73fd64a49a29ced3f296043ee2d
William Budoyo <williambu07@gmail.com>
2019-01-09 17:19:09 -0500

    basic setup is ready

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <h1>My first blog post</h1>
+          <h2>This is my first blog post</h2>
+          <p className="main-text">
+            This is a very <strong> strong</strong>text, because it is <strong> bold</strong> <br></br>
+            I would like to <em>emphasize </em> how important it is to follow this course until the end!<br></br>
+            This is some <u>underlined </u>text.<br></br>
+             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nesciunt doloremque minima illum, quisquam vel nisi itaque unde officiis exercitationem sed quos amet recusandae delectus dolore quia aut laborum vitae.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+          <img src="/images/logo.jpg" alt="The HTML5 logo"/>
+          <a href="http://www.udemy.com">Link to Udemy</a>
+          <a href="/images/logo.jpg" target="_blank">HTML logo</a>
+          <h2> HTML is amazing </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nesciunt doloremque minima illum, quisquam vel nisi itaque unde officiis exercitationem sed quos amet recusandae delectus dolore quia aut laborum vitae.
+         </p>
+         <img src="/images/profilePic.jpeg" alt="profile-pic"/>
+         <p class ="author-text">William Budoyo</p>     
       </div>
     );
   }
