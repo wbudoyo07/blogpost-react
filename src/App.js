c41fda8344fa8a7179675ca162bdfb07b97db9a7
William Budoyo <williambu07@gmail.com>
2019-01-12 19:53:58 -0500

    added a little styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="container">
+          <div className="blog-post">
           <h1>My first blog post</h1>
+          <p className="date"> March 11th, 2015</p>
           <h2>This is my first blog post</h2>
           <p className="main-text">
             This is a very <strong> strong</strong>text, because it is <strong> bold</strong> <br></br>
@@ -19,11 +21,20 @@ class App extends Component {
           <a href="http://www.udemy.com">Link to Udemy</a>
           <a href="/images/logo.jpg" target="_blank">HTML logo</a>
           <h2> HTML is amazing </h2>
-          <p>
+          <p className="main-text">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nesciunt doloremque minima illum, quisquam vel nisi itaque unde officiis exercitationem sed quos amet recusandae delectus dolore quia aut laborum vitae.
          </p>
+         </div>
+         <div className="other-posts">
+         dummy text
+         </div>
+         <div className="clearfix"></div>
+         <div className="author-box">
          <img src="/images/profilePic.jpeg" alt="profile-pic"/>
-         <p class ="author-text">William Budoyo</p>     
+         <p class ="author-text">William Budoyo</p>   
+         </div>
+
+         </div>  
       </div>
     );
   }
